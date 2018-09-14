4efc22806336650d072a85dc35e86197d3943fd8
jimmy3663 <43136526+jimmy3663@users.noreply.github.com>
2018-09-15 03:10:14 +0900

    til Print()

--- 실습과목/Project1/skel_sp_p1/src/p1/Queue.java
+++ 실습과목/Project1/skel_sp_p1/src/p1/Queue.java
@@ -17,24 +17,67 @@ public class Queue {
 	
 	// LOAD, ADD
 	public void Push(WordNode node){
-		
+		if(pHead==null)
+			pHead=node; 
+		else
+		{
+			WordNode tmp;
+			tmp=pHead;
+			
+			while(tmp.GetNext()!=null)
+				tmp=tmp.GetNext();
+			
+			tmp=node; 
+		}
 	}
 	
 	// MOVE
 	public WordNode	Pop()
 	{
-		return null;
+		WordNode tmp;
+		tmp= pHead;
+		pHead=tmp.GetNext();
+		
+		return pHead; 
 	}
 		
 	// SEARCH, UPDATE
 	public WordNode Search(String word)
 	{
+		WordNode tmp;
+		tmp=pHead;
+	
+		if(pHead.GetWord()==word)
+		{
+			return pHead;
+		}
+		
+		else 
+		{
+			while(tmp.GetNext()!=null)
+			{
+				if(tmp.GetWord()==word)
+					return tmp;
+				
+				tmp=tmp.GetNext();
+			}
+		}
+		
 		return null;
 	}
 	
 	// PRINT
 	public boolean Print() throws IOException
 	{
+		WordNode tmp= pHead; 
+		
+		while(tmp.GetNext()!=null)
+		{
+			System.out.printf(tmp.GetWord(), " ");
+			System.out.println(tmp.GetMean());
+			tmp=tmp.GetNext();
+		}
+		
 		return true;
 	}
 	// SAVE
@@ -47,6 +90,13 @@ public class Queue {
 		System.out.println("hello world!");
 		//Manager manager = new Manager();
 		//manager.run("command.txt");
+		
+		Queue test= new Queue();
+		WordNode node = new WordNode();
+		node.SetWord("apple");
+		node.SetMean("»ç°ú");
+		
+		test.Push(node);
 				
 	}
 
