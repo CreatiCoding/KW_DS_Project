99d74ba06bf685afe23fff5d2c17b7bbff357bff
CreatiCoding <33514304+CreatiCoding@users.noreply.github.com>
2018-09-29 05:45:07 +0900

    feat(bst) word bst about delete again.

--- 실습과목/Project1/skel_sp_p1/src/p1/WordBST.java
+++ 실습과목/Project1/skel_sp_p1/src/p1/WordBST.java
@@ -5,39 +5,153 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WordBST {
-	
-	public WordNode root;	// Word BST Root
-		
-	public WordBST()
-	{
-		
+
+	public WordNode root; // Word BST Root
+
+	public WordNode getRoot() {
+		return this.root;
 	}
-	
-	public void Insert(WordNode node)
-	{
-		
-	}
-	
-	public WordNode Delete(String word)
-	{
-		
-	}
-	public WordNode Search(String word)
-	{
-	
-	}
-	public boolean Print() throws IOException
-	{
-		
-	}
-	public boolean Save() throws IOException
-	{
-		
-	}
-	
-	public void	Preorder(WordNode node) throws IOException
-	{
-		
+	public WordBST() {
+		root = null;
 	}
 
+	public void Insert(WordNode node) {
+		if (root == null) {
+			root = node;
+		} else {
+			
+			WordNode tmp = root;
+
+			WordNode target = root, parent = null;
+			
+			while(target!=null && !target.GetWord().equals(word)) {
+				parent = target;
+				target = (target.GetWord().compareTo(word)>0) ? target.GetLeft():target.GetRight();
+			}
+			
+			while (true) {
+				System.out.println("insert\t"+node.GetWord());
+				if (tmp.GetWord().compareTo(node.GetWord()) > 0) {
+					if (tmp.GetLeft() != null) {
+						tmp = tmp.GetLeft();
+					} else {
+						tmp.SetLeft(node);
+						break;
+					}
+				} else if (tmp.GetWord().compareTo(node.GetWord()) < 0) {
+					if (tmp.GetRight() != null) {
+						tmp = tmp.GetRight();
+					} else {
+						tmp.SetRight(node);
+						break;
+					}
+				} else {
+					System.out.println("동일한 단어가 입력되었습니다");
+					return;
+				}
+				return;
+			}
+		}
+	}
+
+	public WordNode Delete(String word) {
+		WordNode target = root, parent = null;
+		while(target!=null && !target.GetWord().equals(word)) {
+			parent = target;
+			target = (target.GetWord().compareTo(word)>0) ? target.GetLeft():target.GetRight();
+		}
+		// not found
+		if(target == null) {
+			return null;
+		}
+		// 리프 노드
+		if(target.GetLeft() == null && target.GetRight() == null) {
+			if(parent == null) {
+				root = null;
+			}else if(parent.GetLeft() == target) {
+				parent.SetLeft(null);
+			}else {
+				parent.SetRight(null);
+			}
+		// 왼쪽이 비어있는 노드
+		} else if(target.GetLeft() == null) {
+			if(parent == null) {
+				root = target.GetRight();
+			}else if(parent.GetLeft() == target) {
+				parent.SetLeft(target.GetRight());
+			}else {
+				parent.SetRight(target.GetRight());
+			}
+		// 오른쪽이 비어있는 노드
+		} else if(target.GetRight() == null) {
+			if(parent == null) {
+				root = target.GetLeft();
+			}else if(parent.GetLeft() == target) {
+				parent.SetLeft(target.GetLeft());
+			}else {
+				parent.SetRight(target.GetLeft());
+			}
+		// 둘다 있는 노드
+		} else {
+			WordNode prevprev = target, prev = target.GetRight(), current = target.GetRight().GetLeft();
+			while(current!=null) {
+				prevprev = prev;
+				prev = current;
+				current = current.GetLeft();
+			}
+			
+			target.SetMean(prev.GetMean());
+			target.SetWord(prev.GetWord());
+			if(prevprev == target) {
+				prevprev.SetRight(prev.GetRight());
+			}else {
+				prevprev.SetLeft(prev.GetRight());
+			}
+		}
+		return target;
+	}
+
+	public WordNode Search(String word) {
+		
+
+		WordNode target = root, parent = null;
+		
+		while(target!=null && !target.GetWord().equals(word)) {
+			parent = target;
+			target = (target.GetWord().compareTo(word)>0) ? target.GetLeft():target.GetRight();
+		}
+		
+		return target;
+	}
+
+	public boolean Print() throws IOException {
+		Preorder(root);
+		return true;
+	}
+
+	public boolean Save() throws IOException {
+		return false;
+	}
+
+	public void Preorder(WordNode node) throws IOException {
+		if (node == null)
+			return;
+		System.out.println("word: " + node.GetWord() +", mean: "+node.GetMean());
+		Preorder(node.GetLeft());
+		Preorder(node.GetRight());
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		WordBST w = new WordBST();
+		w.Insert(new WordNode("apple", "사과"));
+		w.Insert(new WordNode("banana", "바나나"));
+		w.Insert(new WordNode("cat", "고양이"));
+		w.Print();
+		WordNode root = w.getRoot();
+		System.out.println(root.GetWord());
+		System.out.println(root.GetRight().GetWord());
+		System.out.println(root.GetRight().GetRight().GetWord());
+		//System.out.println(root.GetLeft().GetWord());
+	}
 }
--- 실습과목/Project1/skel_sp_p1/src/p1/WordNode.java
+++ 실습과목/Project1/skel_sp_p1/src/p1/WordNode.java
@@ -1,16 +1,23 @@
 package p1;
+
 import p1.Pair;
 
 public class WordNode {
-	
-	private Pair<String,String> wordmean;	//wordmean Pair
-	private WordNode pLeft;					// BST Left Pointer
-	private WordNode pRight;				// BST Right Pointer
-	private WordNode pNext;					// Queue Next Pointer
 
-	WordNode()
-	{
-		this.wordmean = new Pair<String, String>(); 
+	private Pair<String, String> wordmean; // wordmean Pair
+	private WordNode pLeft; // BST Left Pointer
+	private WordNode pRight; // BST Right Pointer
+	private WordNode pNext; // Queue Next Pointer
+
+	WordNode() {
+		this.wordmean = new Pair<String, String>();
+	}
+
+	WordNode(String word, String mean) {
+		this.wordmean = new Pair<String, String>(word, mean);
+		this.pLeft = null;
+		this.pRight = null;
+		this.pNext = null;
 	}
 
 	WordNode(Pair<String, String> wordmean, WordNode pLeft, WordNode pRight, WordNode pNext) {
@@ -20,44 +27,43 @@ public class WordNode {
 		this.pNext = pNext;
 	}
 
-	public String GetWord()
-	{
+	public String GetWord() {
 		return wordmean.first;
 	}
-	public String GetMean()
-	{
+
+	public String GetMean() {
 		return wordmean.second;
 	}
-	public WordNode GetLeft()
-	{
+
+	public WordNode GetLeft() {
 		return pLeft;
 	}
-	public WordNode GetRight()
-	{
+
+	public WordNode GetRight() {
 		return pRight;
 	}
-	public WordNode GetNext()
-	{
+
+	public WordNode GetNext() {
 		return pNext;
 	}
-	public void	SetWord(String word)
-	{
+
+	public void SetWord(String word) {
 		this.wordmean.first = word;
 	}
-	public void SetMean(String mean)
-	{
+
+	public void SetMean(String mean) {
 		this.wordmean.second = mean;
 	}
-	public void	SetLeft(WordNode node)
-	{
-		this.pLeft=node;
+
+	public void SetLeft(WordNode node) {
+		this.pLeft = node;
 	}
-	public void	SetRight(WordNode node)
-	{
-		this.pRight=node;
+
+	public void SetRight(WordNode node) {
+		this.pRight = node;
 	}
-	public void	SetNext(WordNode node)
-	{
-		this.pNext=node;
-	}	
+
+	public void SetNext(WordNode node) {
+		this.pNext = node;
+	}
 }
