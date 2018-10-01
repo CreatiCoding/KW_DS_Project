c7904b8eef666f8bafc86f3932d5045c127b79f5
JeongSeokHo <creaticoding@gmail.com>
2018-10-01 20:50:55 +0900

    feat(queue): add about initial pHead, Print first node error fix.

--- 실습과목/Project1/skel_sp_p1/src/p1/Queue.java
+++ 실습과목/Project1/skel_sp_p1/src/p1/Queue.java
@@ -1,12 +1,7 @@
 package p1;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 
 import p1.WordNode;
@@ -16,16 +11,17 @@ public class Queue {
 	public WordNode pHead;
 
 	Queue() {
-
+		pHead = null;
 	}
 
 	// LOAD, ADD
 	public void Push(WordNode node) {
-		if (pHead == null)
+		if(this.Search(node.GetWord()) != null)
+			return ;
+		else if (pHead == null)
 			pHead = node;
 		else {
 			WordNode tmp = pHead;
-
 			while (tmp.GetNext() != null)
 				tmp = tmp.GetNext();
 			
@@ -47,44 +43,47 @@ public class Queue {
 		WordNode tmp;
 		tmp = pHead;
 
-		if (pHead.GetWord() == word) {
+		if(pHead == null) {
+			return null;
+		}else if (pHead.GetWord() == word) {
 			return pHead;
 		}
 
 		else {
 			while (tmp.GetNext() != null) {
-				if (tmp.GetWord() == word)
+				if (word.equals(tmp.GetWord()))
 					return tmp;
-
 				tmp = tmp.GetNext();
 			}
 		}
-
 		return null;
 	}
 
 	// PRINT
 	public boolean Print() throws IOException {
 		WordNode tmp = pHead;
-
 		if (pHead == null) {
 			System.out.println("No Word in the wordlist");
 			return false;
 		} else {
-
-			System.out.printf(tmp.GetWord(), " ");
-			System.out.println(tmp.GetMean());
 			while (tmp.GetNext() != null) {
 				System.out.printf(tmp.GetWord(), " ");
 				System.out.println(tmp.GetMean());
 				tmp = tmp.GetNext();
 			}
-
+			System.out.printf(tmp.GetWord(), " ");
+			System.out.println(tmp.GetMean());
 			return true;
 		}
-
 	}
 
+	public String toString()  {
+		try {
+			return this.toString(pHead);
+		} catch (IOException e) {
+			return null;
+		}
+	}
 	public String toString(WordNode node) throws IOException {
 		if (node == null)
 			return "";
@@ -98,7 +97,7 @@ public class Queue {
 
 		BufferedOutputStream bos;
 
-		// 쓸 문자열 = 이전 내용 + 작성한 내용
+		// 쓸 문자열 = 작성한 내용
 		String contents = this.toString(this.pHead);
 
 		// 파일 쓰기
@@ -114,13 +113,16 @@ public class Queue {
 		System.out.println("hello world!");
 
 		Queue test = new Queue();
-		test.Push(new WordNode("apple", "사과"));
-		test.Push(new WordNode("banana", "바나"));
-		test.Push(new WordNode("cat", "고양이"));
+		test.Push(new WordNode("banana", "바나나"));
+		test.Push(new WordNode("boat", "배"));
+		test.Push(new WordNode("book", "책"));
 		test.Push(new WordNode("doctor", "의사"));
-		test.Push(new WordNode("eagle", "독수리"));
-		test.Print();
+		test.Push(new WordNode("dress", "드레스"));
+		test.Push(new WordNode("dear", "친애하는"));
+		
 		test.Save();
+		System.out.println(test.toString());
+		test.Print();
 	}
 
 }
