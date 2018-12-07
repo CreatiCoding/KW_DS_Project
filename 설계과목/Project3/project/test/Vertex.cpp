bbd2447f289f4ea7bd1efa0a3858b600415db350
creaticoding <creaticoding@gmail.com>
2018-12-07 23:51:48 +0900

    fix(DS): fix cpp class implementation with namespace

--- 설계과목/Project3/project/test/Vertex.cpp
+++ 설계과목/Project3/project/test/Vertex.cpp
@@ -1,69 +1,85 @@
 #include "Vertex.h"
 
-    /**
+/**
      *  int m_key;
      *  int m_size;
      *  Edge* m_pEHead;
      *  Vertex* m_pNext;
     */
-    
-    Vertex(){
 
-    }
-    Vertex(int key){
-        m_key = key;
-    }
-    ~Vertex(){
-        Clear();
-    }
+Vertex::Vertex(){};
+Vertex::Vertex(int key)
+{
+    m_key = key;
+}
+Vertex::~Vertex()
+{
+    Clear();
+}
 
-    void SetNext(Vertex* pNext){
-        m_pNext = pNext;
-    }
-    int GetKey() const{
-        return m_key;
-    }
-    Vertex* GetNext() const{
-        return m_pNext;
-    }
+void Vertex::SetNext(Vertex *pNext)
+{
+    m_pNext = pNext;
+}
+int Vertex::GetKey() const
+{
+    return m_key;
+}
+Vertex *Vertex::GetNext() const
+{
+    return m_pNext;
+}
 
-    int Size() const{
-        return m_size;
+int Vertex::Size() const
+{
+    return m_size;
+}
+
+void Vertex::AddEdge(int edgeKey, int weight)
+{
+    Edge *newE = new Edge(edgeKey, weight);
+    Edge *curr = m_pEHead;
+    Edge *prev = NULL;
+
+    if (curr == NULL)
+    {
+        curr = newE;
     }
-
-    void AddEdge(int edgeKey, int weight){
-        Edge *newE = new Edge(key, weight);
-        Edge *curr = m_pEHead;
-        Edge *prev = NULL;
-
-        if(curr == NULL){
-            curr = e;
-        }else{
-            prev = curr;
-            while(curr != NULL){
-                if(curr->GetKey() < newE->GetKey()){
-                    curr = curr->GetNext();
-                }else{
-                    prev->SetNext(newE);
-                    newE->SetNext(curr);
-                    break;
-                }
+    else
+    {
+        prev = curr;
+        while (curr != NULL)
+        {
+            if (curr->GetKey() < newE->GetKey())
+            {
+                curr = curr->GetNext();
             }
-            if(curr == NULL){
+            else
+            {
                 prev->SetNext(newE);
+                newE->SetNext(curr);
+                break;
             }
         }
-    }
-    
-    Edge* GetHeadOfEdge() const{
-        return m_pEHead;
-    }
-    /// memory free for edges
-    void Clear(){
-        Edge *curr = m_pEHead;
-        while(curr != NULL){
-            Edge *willDel = curr;
-            curr = curr->GetNext();
-            delete willDel;
+        if (curr == NULL)
+        {
+            prev->SetNext(newE);
         }
     }
+}
+
+Edge *Vertex::GetHeadOfEdge() const
+{
+    return m_pEHead;
+}
+/// memory free for edges
+void Vertex::Clear()
+{
+    Edge *curr = m_pEHead;
+    while (curr != NULL)
+    {
+        Edge *willDel = curr;
+        curr = curr->GetNext();
+        delete willDel;
+    }
+}
